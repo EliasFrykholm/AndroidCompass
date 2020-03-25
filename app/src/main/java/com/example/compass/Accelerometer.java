5247d6ee5e0ee064bfd55a1f2afd68cb431e8c2c
Frykis <el6426fr-s@student.lu.se>
2020-03-25 16:41:54 +0100

    Level mode for accelerometer implemented

--- app/src/main/java/com/example/compass/Accelerometer.java
+++ app/src/main/java/com/example/compass/Accelerometer.java
@@ -7,6 +7,10 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,12 +21,18 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
     TextView value_X;
     TextView value_Y;
     TextView value_Z;
+    int newMarginX;
+    int newMarginY;
+    ImageView marker;
+    int gridWidth;
+    FrameLayout markerView;
     DecimalFormat df = new DecimalFormat("#.###");
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
     boolean haveSensor = false;
     private float[] mLastAccelerometer = new float[3];
     boolean mLastAccelerometerSet = false;
+    FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +43,15 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
         value_X = findViewById(R.id.value_X);
         value_Y = findViewById(R.id.value_Y);
         value_Z = findViewById(R.id.value_Z);
+        marker = findViewById(R.id.marker);
+        markerView = findViewById(R.id.marker_grid);
+        markerView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                markerView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                gridWidth = markerView.getWidth();
+            }
+        });
 
         start();
     }
@@ -47,6 +66,10 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
         value_X.setText(df.format(mLastAccelerometer[0]));
         value_Y.setText(df.format(mLastAccelerometer[1]));
         value_Z.setText(df.format(mLastAccelerometer[2]));
+        newMarginX = (int) (gridWidth/2 - 40 + mLastAccelerometer[0]*gridWidth/20);
+        newMarginY = (int) (gridWidth/2 - 40 + mLastAccelerometer[1]*gridWidth/20);
+        lp.setMargins(newMarginX, newMarginY, 0, 0);
+        marker.setLayoutParams(lp);
     }
 
     @Override
